015fed163109d79477e4f4ef1e5e078c6de4daa9
Extrieve <nicolas.cinera@gmail.com>
2023-01-16 15:17:57 -0500

    Table for champions

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,43 @@ function App() {
 
   const [champions, setChampions] = useState([])
 
-  // Fetch data from API
+  // Async useEffect
   useEffect(() => {
-    fetch('http://localhost:8080/champions')
-      .then(response => response.json())
-      .then(data => setChampions(data)).catch(error => console.log(error))
+    fetchChampions()
   }, [])
+
+  async function fetchChampions() {
+    const response = await fetch('http://localhost:8080/champions')
+    const data = await response.json()
+    setChampions(data)
+  }
 
   return (
     <div> 
       <Header title={'League of Babes'}/>
-      {champions.map((champion: any) => (
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Picture</th>
+          </tr>
+        
+        </thead>
+        <tbody>
+          {champions.map((champion: any) => (
+            <tr>
+              <td>{champion['name']}</td>
+              <td><img src={champion['profilePictureUrl']} alt=""/></td>
+            </tr>
+          )
+          )}
+        </tbody>
+
+      </table>
+      {/* {champions.map((champion: any) => (
         <Card championName={champion['name']} championPicture={champion['profilePictureUrl']} />
       )
-      )}
+      )} */}
       <button className='btn btn-primary' onClick={() => setCount(count + 1)}>Press me daddy {count}</button>
     </div>
   )
