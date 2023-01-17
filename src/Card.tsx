1fd19c1a1434f4c074be3ce51e081be98c4a6075
Extrieve <nicolas.cinera@gmail.com>
2023-01-17 10:41:02 -0500

    Auto size for Cards

--- src/Card.tsx
+++ src/Card.tsx
@@ -7,7 +7,7 @@ type HeaderProps = {
 
 const Card : FC<HeaderProps> = ({championName, championPicture}) => {
     return(
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card" style={{width: "auto"}}>
         <img src={championPicture} className="card-img-top" alt="..." />
         <div className="card-body">
             <h5 className="card-title">{championName}</h5>
