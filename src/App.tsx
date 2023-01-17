1094fbde6622ae2bd6c5b2f0e2a713f4d704bd24
Extrieve <nicolas.cinera@gmail.com>
2023-01-17 09:40:22 -0500

    Experimenting

--- src/App.tsx
+++ src/App.tsx
@@ -20,26 +20,37 @@ function App() {
     setChampions(data)
   }
 
+  // Devide champions into 2 arrays
+  const champions1 = champions.slice(0, champions.length / 2)
+  const champions2 = champions.slice(champions.length / 2, champions.length)
+
   return (
     <div> 
       <Header title={'League of Babes'}/>
       <button className='btn btn-danger' onClick={() => setCount(count + 1)}>Press me daddy {count}</button>
       <table className="table table-striped">
-        <thead>
+        {/* <thead>
           <tr>
             <th scope="col">Name</th>
             <th scope="col">Picture</th>
           </tr>
         
-        </thead>
+        </thead> */}
         <tbody>
-          {champions.map((champion: any) => (
-            <tr>
-              <td>{champion['name']}</td>
-              <td><img src={champion['profilePictureUrl']} alt="" style={{"height": "15%", "width": "15%"}} /></td>
-            </tr>
-          )
-          )}
+          <tr>
+            <td>
+              {champions1.map((champion: any) => (
+                <Card championName={champion['name']} championPicture={champion['profilePictureUrl']} />
+              )
+              )}
+            </td>
+            <td>
+              {champions2.map((champion: any) => (
+                <Card championName={champion['name']} championPicture={champion['profilePictureUrl']} />
+              )
+              )}
+            </td>
+          </tr>
         </tbody>
 
       </table>
