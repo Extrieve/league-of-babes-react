000387a1f330abf8d185b0d69f0329d1a295c9b9
Extrieve <nicolas.cinera@gmail.com>
2023-01-21 10:07:27 -0500

    New Structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import './App.css'
 import Home from './pages/Home'
 import Match from './pages/Match'
 import Create from './pages/Create'
+import Champions from './pages/Champions'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/champions" element={<Champions />} />
       <Route path="/match" element={<Match />} />
       <Route path="/create" element={<Create />} />
     </Routes>
