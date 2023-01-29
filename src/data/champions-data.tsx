e4a1a0e174891a0e8de0979ce7a013cd16622665
Extrieve <nicolas.cinera@gmail.com>
2023-01-28 19:29:27 -0500

    Is this how you do it?

---
+++ src/data/champions-data.tsx
@@ -0,0 +1,50 @@
+export interface iChampion {
+    name: string,
+    profilePictureUrl: string
+}
+
+const fetchAllChampions = async () => {
+    const response = await fetch('http://localhost:3000/champions')
+    const data = await response.json()
+    return data
+}
+
+const fetchChampion = async (name: string) => {
+    const response = await fetch(`http://localhost:3000/champions/${name}`)
+    const data = await response.json()
+    return data
+}
+
+const createChampion = async (champion: iChampion) => {
+    const response = await fetch('http://localhost:3000/champions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(champion)
+    })
+    const data = await response.json()
+    return data
+}
+
+const updateChampion = async (champion: iChampion) => {
+    const response = await fetch(`http://localhost:3000/champions/${champion.name}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(champion)
+    })
+    const data = await response.json()
+    return data
+}
+
+const deleteChampion = async (name: string) => {
+    const response = await fetch(`http://localhost:3000/champions/${name}`, {
+        method: 'DELETE'
+    })
+    const data = await response.json()
+    return data
+}
+
+export default { fetchAllChampions, fetchChampion, createChampion, updateChampion, deleteChampion }
