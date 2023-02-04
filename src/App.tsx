f69a605e4fa4b7f0fc16f5123d1df726b7fa9145
Nick <nicolas.cinera@gmail.com>
2023-02-03 21:17:20 -0500

    Ugh

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,22 @@ import Match from './pages/Match'
 import Create from './pages/Create'
 import Champions from './pages/Champions'
 import Nav from './components/Nav'
-import AllChampions from './data/AllChampions.json'
-import { iChampion } from './interfaces/iChampion'
-
+import fetchAllChampions from './data/champions-data'
+import { iChampion } from './data/champions-data'
 
 function App() {
 
   const [champions, setChampions] = useState<iChampion[]>([])
-  // const [champions, setChampions] = useState([])
-  const fetchChampions = () => {
-    setChampions(AllChampions)
-  }
 
   // Async useEffect
   useEffect(() => {
+    // Fetch all champions
+    const fetchChampions = async () => {
+      const data = await fetchAllChampions.fetchAllChampions()
+      setChampions(data)
+    }
     fetchChampions()
   }, [])
-
-  // async function fetchChampions() {
-  //   const response = await fetch('http://localhost:8080/champions')
-  //   const data = await response.json()
-  //   setChampions(data)
-  // }
-  
 
 
   return (
